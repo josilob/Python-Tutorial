2e92b94e02b1d9fa4d8f25dfaa10bcc5fd1ed793
Bojan Josilo <bjosilo@gmail.com>
2022-01-13 11:37:09 -0500

    string methods

--- intro.py
+++ intro.py
@@ -28,19 +28,38 @@ course = "Python's course for beginners"  # DBL && Single combo
 course = '''Multiline
 string with three apostrophes'''
 course = 'Python for beginners'  # single quoted string
-print(course[0])  # first char 'P'
-print(course[-1])  # last char 's'
-print(course[0:3])  # first 3 characters (0 inclusive, 3 exclusive) 'Pyt'
+# print(course[0])  # first char 'P'
+# print(course[-1])  # last char 's'
+# print(course[0:3])  # first 3 characters (0 inclusive, 3 exclusive) 'Pyt'
 # omitting first element is set by default to 0, while second element is replaced with last element
-print(course[0:])  # from 0 to string's length (whole string)
-print(course[:5])  # first 5 characters
+# print(course[0:])  # from 0 to string's length (whole string)
+# print(course[:5])  # first 5 characters
 name = 'Jennifer'
-print(name[1:-1])  # => 'ennife'
+# print(name[1:-1])  # => 'ennife'
 
 # Formatted Strings
 first = 'John'
 last = 'Doe'
 message = first + ' [' + last + '] is a coder'
-print(message)  # => John [Doe] is a coder
+# print(message)  # => John [Doe] is a coder
 msg = f'{first} [{last}] is a coder'
-print(msg)  # => Same output
+# print(msg)  # => Same output
+
+# String methods
+
+course = 'Python for beginners'
+print(len(course))
+print(course.upper())  # => Does not modify current string
+print(course.lower())  # => Does not modify current string
+
+# Case sensitive find method that returns first index of string occurence or -1
+print(course.find('Pyt'))  # => 0
+print(course.find('o'))  # => 4
+print(course.find('x'))  # => -1
+print(course.replace('beginners', 'absolute beginners'))
+
+# Check for existance of character or sequence in a string
+print('Python' in course)  # => True
+
+print(course.capitalize())  # => Capitalizes first letter of a string
+print(course.title())  # => Capitalizes first letter of each word in a string
